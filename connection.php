355a2af4f758a1ddb0df3f6e5e7044b306a0ba58
dhruvmpatel03 <152317414+dhruvmpatel03@users.noreply.github.com>
2024-05-08 18:47:41 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+// session_start();
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "yoga";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+// echo "connection successful 😁";
+?>
